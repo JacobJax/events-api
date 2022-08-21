b087c6a80109cde1499b0eab24c623ae4419227c
Jacob Odero <jackjax617@gmail.com>
2022-08-21 12:20:30 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use('/events', eventsRouter)
 // connect to db
 mongoose.connect(process.env.DB_URI)
 
-app.listen(process.env.DB_URI || 8081)
+app.listen(process.env.PORT || 8081)
 
 // export app for testing
 module.exports = app
